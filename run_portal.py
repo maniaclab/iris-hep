db3c528674b8317e1c5f09c2db2d72cb9d580453
Andrew Taylor <rolyata47@gmail.com>
2022-11-29 12:52:37 -0600

    Updating run_portal.py

--- run_portal.py
+++ run_portal.py
@@ -1,4 +1,6 @@
+#!/usr/bin/env python
+
 from portal import app
 
 if __name__ == "__main__":
-    app.run(host="localhost", ssl_context=("./ssl/cert.pem", "./ssl/key.pem"))
+    app.run(host='0.0.0.0', port=8080)
